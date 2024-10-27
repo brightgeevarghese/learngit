8bef19cfc9d7440795f5ac4edc015ee85647fd4d
Bright Gee Varghese <brightlsom@gmail.com>
2024-10-27 16:27:00 -0500

    Update MyHello.java

--- MyHello.java
+++ MyHello.java
@@ -2,5 +2,6 @@ class MyHello {
     public static void main(String[] args) {
         System.out.println("My hello!");
         System.out.println("Second line");
+        System.out.println("Third line by someone");
     }
 }
