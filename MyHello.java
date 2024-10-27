cf4c6083bb152b2b79842859fe53e605475f4599
Bright Gee Varghese <brightlsom@gmail.com>
2024-10-27 16:26:10 -0500

    Added a for loop

--- MyHello.java
+++ MyHello.java
@@ -2,5 +2,8 @@ class MyHello {
     public static void main(String[] args) {
         System.out.println("My hello!");
         System.out.println("Second line");
+        for(int i = 0; i < 2; i++) {
+            System.out.println("hello");
+        }
     }
 }
